14bb9c84519b2b39e2c60cdc7730b30588223d05
liufei <1583409404@qq.com>
2021-05-15 09:40:02 +0800

    11. 盛最多水的容器 -> 滑动窗口

--- leetcode/src/main/java/com/liufei/top100/Top011_middle.java
+++ leetcode/src/main/java/com/liufei/top100/Top011_middle.java
@@ -34,14 +34,19 @@ public class Top011_middle {
 
     public static void main(String[] args) {
         Top011_middle top11 = new Top011_middle();
-        int[] height = { 4,3,2,1,4};
-        int result = top11.maxArea(height);
+        int[] height = { 1,2,1 };
+        int result = top11.maxArea2(height);
         System.out.println(result);
     }
 
-
+    /**
+     * 暴力法，超出了时间限制
+     * O(n^2)
+     * @param height
+     * @return
+     */
     public int maxArea(int[] height) {
-        int result = height[0];
+        int result = 0;
         int len = height.length;
         for (int i = 0; i < len - 1; i++) {
             for (int j = i + 1; j < len; j++) {
@@ -50,5 +55,29 @@ public class Top011_middle {
             }
         }
         return result;
+    }
+
+    /**
+     * 滑动窗口，最优解
+     * 基本的表达式: area = min(height[i], height[j]) * (j - i) 使用两个指针，值小的指针向内移动，
+     * 这样就减小了搜索空间 因为面积取决于指针的距离与值小的值乘积，
+     * 如果值大的值向内移动，距离一定减小，而求面积的另外一个乘数一定小于等于值小的值，因此面积一定减小，
+     * 而我们要求最大的面积，因此值大的指针不动，而值小的指针向内移动遍历
+     * @param height
+     * @return
+     */
+    public int maxArea2(int[] height) {
+        int len = height.length;
+        if (len <= 1) return -1;
+        int left = 0, right = len - 1, res = 0;
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            res = Math.max(res, h * (right - left));
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return res;
     }
 }
