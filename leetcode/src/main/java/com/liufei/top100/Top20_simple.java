d353d098efbd947dfbc45052d075897b2b000508
liufei <1583409404@qq.com>
2021-05-30 16:53:27 +0800

    20. 有效的括号

---
+++ leetcode/src/main/java/com/liufei/top100/Top20_simple.java
@@ -0,0 +1,92 @@
+package com.liufei.top100;
+
+import java.util.*;
+
+/**
+ * 20. 有效的括号  [ https://leetcode-cn.com/problems/valid-parentheses/ ]
+ * <p>
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "()"
+ * 输出：true
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：s = "(]"
+ * 输出：false
+ * <p>
+ * 示例 4：
+ * <p>
+ * 输入：s = "([)]"
+ * 输出：false
+ * <p>
+ * 示例 5：
+ * <p>
+ * 输入：s = "{[]}"
+ * 输出：true
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 104
+ * s 仅由括号 '()[]{}' 组成
+ */
+public class Top20_simple {
+
+
+    public static void main(String[] args) {
+        Top20_simple top20 = new Top20_simple();
+        String s = "{[]}";
+        boolean valid = top20.isValid2(s);
+        System.out.println(valid);
+    }
+
+    /**
+     * 效率高。官方答案
+     * 时间复杂度：O(n)，其中 nn 是字符串 ss 的长度。
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                stack.push(')');
+            } else if (ch == '{') {
+                stack.push('}');
+            } else if (ch == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || ch != stack.pop()) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    /**
+     * 效率不高
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid2(String s) {
+        int length = s.length() / 2;
+        for (int i = 0; i < length; i++) {
+            s = s.replace("()", "").replace("{}", "").replace("[]", "");
+        }
+        return s.length() == 0;
+    }
+}
