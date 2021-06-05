d9af2bb6d3a3a49cc6226f2e6f156c60945ba0f1
liufei <1583409404@qq.com>
2021-06-05 18:26:54 +0800

    22. K 个一组翻转链表

--- leetcode/src/main/java/com/liufei/top100/Top025_hard.java
+++ leetcode/src/main/java/com/liufei/top100/Top025_hard.java
@@ -2,35 +2,37 @@ package com.liufei.top100;
 
 import com.liufei.model.ListNode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 import java.util.Stack;
 
 /**
  * 25. K 个一组翻转链表 [ https://leetcode-cn.com/problems/reverse-nodes-in-k-group/ ]
- *
+ * <p>
  * 给你一个链表，每k个节点一组进行翻转，请你返回翻转后的链表。
- *
+ * <p>
  * k是一个正整数，它的值小于或等于链表的长度。
- *
+ * <p>
  * 如果节点总数不是k的整数倍，那么请将最后剩余的节点保持原有顺序。
- *
+ * <p>
  * 进阶：
- *
+ * <p>
  * 你可以设计一个只使用常数额外空间的算法来解决此问题吗？
  * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
- *
+ * <p>
  * 示例 1：
  * 输入：head = [1,2,3,4,5], k = 2
  * 输出：[2,1,4,3,5]
- *
+ * <p>
  * 示例 2：
  * 输入：head = [1,2,3,4,5], k = 3
  * 输出：[3,2,1,4,5]
- *
+ * <p>
  * 示例 3：
  * 输入：head = [1,2,3,4,5], k = 1
  * 输出：[1,2,3,4,5]
- *
+ * <p>
  * 示例 4：
  * 输入：head = [1], k = 1
  * 输出：[1]
@@ -41,42 +43,109 @@ public class Top025_hard {
     public static void main(String[] args) {
         Top025_hard top025 = new Top025_hard();
         ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-        ListNode result = top025.reverseKGroup(listNode, 5);
+        //ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3)));
+        ListNode result = top025.reverseKGroup2(listNode, 3);
         System.out.println(result);
     }
 
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (k < 2) {
-            return head;
-        }
-        ListNode cur = head;
-        Stack<ListNode> stack = new Stack<>();
-        for (int i = 1; i <= k; i++) {
-            if (i != k) {
-                stack.push(cur);
-                cur = cur.next;
-            } else {
-                ListNode tmp = cur;
-                while (!stack.isEmpty()) {
-                    // 4 5
-                    ListNode pre = stack.pop();
-                    tmp.next = pre;
-                    tmp = tmp.next;
-                }
-            }
-        }
-        return head;
-    }
-
-    public ListNode reverse(ListNode head, int k) {
+        Deque<ListNode> stack = new ArrayDeque();
         ListNode dummy = new ListNode(0);
-        ListNode cur = head;
-        for (int i = 0; i < k; i++) {
-            ListNode next = cur.next;
-            cur.next = dummy.next;
-            dummy.next = cur;
-            cur = next;
+        ListNode cur = dummy;
+        while (true) {
+            ListNode tmp = head;
+            int count = 0;
+            while (tmp != null && count < k) {
+                stack.add(tmp);
+                tmp = tmp.next;
+                count++;
+            }
+            if (count != k) {
+                break;
+            }
+
+            while (!stack.isEmpty()) {
+                cur.next = stack.pollLast();
+                cur = cur.next;
+            }
+
+            cur.next = tmp;
+            head = tmp;
         }
         return dummy.next;
+    }
+
+
+    public ListNode reverseKGroup2(ListNode head, int k) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode pre = dummy;
+        ListNode cur = head;
+        ListNode next;
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+
+        for (int i = 0; i < len / k; i++) {
+            for (int j = 0; j < k - 1; j++) {
+                next = cur.next;
+                cur.next = next.next;
+                next.next = pre.next;
+                pre.next = next;
+            }
+            pre = cur;
+            cur = pre.next;
+        }
+        return dummy.next;
+    }
+
+    /**
+     * 官方答案
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup3(ListNode head, int k) {
+        ListNode hair = new ListNode(0);
+        hair.next = head;
+        ListNode pre = hair;
+        while (head != null) {
+            ListNode tail = pre;
+            // 查看剩余长度是否大于等于K
+            for (int i = 0; i < k; i++) {
+                tail = tail.next;
+                if (tail == null) {
+                    return hair.next;
+                }
+            }
+
+            ListNode next = tail.next;
+            ListNode[] reverse = myReverse(head, tail);
+            head = reverse[0];
+            tail = reverse[1];
+
+            // 把子链表重新接回原链表
+            pre.next = head;
+            tail.next = next;
+            pre = tail;
+            head = tail.next;
+
+        }
+
+        return hair.next;
+    }
+
+    public ListNode[] myReverse(ListNode head, ListNode tail) {
+        ListNode prev = tail.next;
+        ListNode p = head;
+        while (prev != tail) {
+            ListNode nex = p.next;
+            p.next = prev;
+            prev = p;
+            p = nex;
+        }
+        return new ListNode[]{tail, head};
     }
 }
