cf0ae930e80c141d9efadae1bcfc1bce081102d6
liufei <1583409404@qq.com>
2021-07-11 22:48:38 +0800

    数组排序2

---
+++ leetcode/src/main/java/com/liufei/top100/Top038_middle.java
@@ -0,0 +1,23 @@
+package com.liufei.top100;
+
+/**
+ * 38. 外观数列 [ https://leetcode-cn.com/problems/count-and-say/ ]
+ *
+ * 给定一个正整数 n ，输出外观数列的第 n 项。
+ *
+ * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+ *
+ * 你可以将其视作是由递归公式定义的数字字符串序列：
+ *
+ * countAndSay(1) = "1"
+ * countAndSay(n) 是对 countAndSay(n-1) 的描述，然后转换成另一个数字字符串。
+ * 前五项如下：
+ *
+ */
+public class Top038_middle {
+
+    public static void main(String[] args) {
+        Top038_middle top038_middle = new Top038_middle();
+
+    }
+}
