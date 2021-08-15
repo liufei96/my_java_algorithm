c1d6ebd29286f266046b413aecb73a7ec7c8ea6e
liufei <1583409404@qq.com>
2021-08-15 22:42:32 +0800

    43. 字符串相乘

--- leetcode/src/main/java/com/liufei/top100/Top010_hard.java
+++ leetcode/src/main/java/com/liufei/top100/Top010_hard.java
@@ -4,36 +4,35 @@ import java.util.Scanner;
 
 /**
  * 正则表达式匹配 [ https://leetcode-cn.com/problems/regular-expression-matching/ ]
- *
+ * <p>
  * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
  * '.' 匹配任意单个字符
  * '*' 匹配零个或多个前面的那一个元素
  * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
- *
+ * <p>
  * 示例 1：
  * 输入：s = "aa" p = "a"
  * 输出：false
  * 解释："a" 无法匹配 "aa" 整个字符串。
- *
+ * <p>
  * 示例 2:
  * 输入：s = "aa" p = "a*"
  * 输出：true
  * 解释：因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
- *
+ * <p>
  * 示例 3：
  * 输入：s = "ab" p = ".*"
  * 输出：true
  * 解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
- *
+ * <p>
  * 示例 4：
  * 输入：s = "aab" p = "c*a*b"
  * 输出：true
  * 解释：因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
- *
+ * <p>
  * 示例 5：
  * 输入：s = "mississippi" p = "mis*is*p*."
  * 输出：false
- *
  */
 public class Top010_hard {
 
@@ -41,6 +40,7 @@ public class Top010_hard {
         // TODO
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
+        System.out.println(num);
     }
 
 
@@ -49,22 +49,41 @@ public class Top010_hard {
         int pLen = p.length();
         if (sLen == pLen && pLen == 0) return true;
 
-        boolean[][] dp = new boolean[sLen+1][pLen+1];
+        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
         //dp[i][j]表示s的0到i-1和p的0到j-1是否匹配
         dp[0][0] = true;
 
         // 初始化s=    0
-        for (int j = 0; j <= pLen ; j++) {
+        for (int i = 0; i <= sLen; i++) {
             //当s为空时，a*b*c*可以匹配
             //当判断到下标j-1是*，j-2是b，b对应f，要看之前的能否匹配
             //比如a*b*下标依次为ftft，b之前的位t，所以j-1也是true
             //即dp[0][j]对应的下标j-1位true
-
+            for (int j = 1; j <= pLen; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }
+            }
         }
-
-        return false;
+        return dp[sLen][pLen];
     }
 
+    public boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
 
     public boolean isMatch2(String s, String p) {
         return s.matches(p);
