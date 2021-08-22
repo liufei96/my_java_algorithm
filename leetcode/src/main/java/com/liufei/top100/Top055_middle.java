7f82cb6ee05300429e2d71599799172a0ca578ce
liufei <1583409404@qq.com>
2021-08-22 21:16:07 +0800

    55. 跳跃游戏，增加注释

--- leetcode/src/main/java/com/liufei/top100/Top055_middle.java
+++ leetcode/src/main/java/com/liufei/top100/Top055_middle.java
@@ -52,12 +52,12 @@ public class Top055_middle {
      */
     public boolean canJump(int[] nums) {
         int n = nums.length - 1;
-        int maxPosition = n;
+        int position = n;
         for (int i = n - 1; i >= 0; i--) {
-            if (nums[i] + i >= maxPosition) {
-                maxPosition = i;
+            if (nums[i] + i >= position) {
+                position = i;
             }
         }
-        return maxPosition == 0;
+        return position == 0;
     }
 }
