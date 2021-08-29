910a64fa31f22c62bde7195b16623d45765ad4f6
liufei <1583409404@qq.com>
2021-08-29 17:40:53 +0800

    62. 不同路径

---
+++ leetcode/src/main/java/com/liufei/top100/Top062_middle.java
@@ -0,0 +1,84 @@
+package com.liufei.top100;
+
+/**
+ * 62. 不同路径   [ https://leetcode-cn.com/problems/unique-paths/ ]
+ * <p>
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * <p>
+ * 问总共有多少条不同的路径？
+ * <p>
+ * 示例 1：
+ * 输入：m = 3, n = 7
+ * 输出：28
+ * <p>
+ * 示例 2：
+ * 输入：m = 3, n = 2
+ * 输出：3
+ * 解释：
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向下
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：m = 7, n = 3
+ * 输出：28
+ * 示例 4：
+ * <p>
+ * 输入：m = 3, n = 3
+ * 输出：6
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= m, n <= 100
+ * 题目数据保证答案小于等于 2 * 109
+ */
+public class Top062_middle {
+
+    public static void main(String[] args) {
+        Top062_middle top062_middle = new Top062_middle();
+        int m = 7, n = 3;
+        int res = top062_middle.uniquePaths(m, n);
+        System.out.println(res);
+        System.out.println(top062_middle.uniquePaths2(m, n));
+    }
+
+
+    /**
+     * 动态规划
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /**
+     * 组合数学
+     * 从左上角到右下角的过程中，我们需要移动 m+n-2 次，其中有 m-1 次向下移动，n-1 次向右移动。因此路径的总数，就等于从 m+n-2 次移动中选择 m-1 次向下移动的方案数，即组合数：
+     */
+
+    public int uniquePaths2(int m, int n) {
+        long ans = 1;
+        for (int i = n, j = 1; j < m; i++, j++) {
+            ans = ans * i / j;
+        }
+        return (int) ans;
+    }
+
+}
