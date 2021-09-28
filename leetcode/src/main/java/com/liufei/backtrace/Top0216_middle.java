28ca9cd3b60a1b963306862742493d9d4d7bda4c
liufei <1583409404@qq.com>
2021-09-29 00:11:19 +0800

    组合总和 III

---
+++ leetcode/src/main/java/com/liufei/backtrace/Top0216_middle.java
@@ -0,0 +1,59 @@
+package com.liufei.backtrace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 216. 组合总和 III   [ https://leetcode-cn.com/problems/combination-sum-iii/ ]
+ * <p>
+ * 找出所有相加之和为 n 的 k 个数的组合。组合中只允许含有 1 - 9 的正整数，并且每种组合中不存在重复的数字。
+ * <p>
+ * 说明：
+ * <p>
+ * 所有数字都是正整数。
+ * 解集不能包含重复的组合。
+ * 示例 1:
+ * <p>
+ * 输入: k = 3, n = 7
+ * 输出: [[1,2,4]]
+ * 示例 2:
+ * <p>
+ * 输入: k = 3, n = 9
+ * 输出: [[1,2,6], [1,3,5], [2,3,4]]
+ */
+public class Top0216_middle {
+
+    public static void main(String[] args) {
+        Top0216_middle top0216_middle = new Top0216_middle();
+        int k = 3, n = 9;
+        List<List<Integer>> lists = top0216_middle.combinationSum3(k, n);
+        System.out.println(lists);
+    }
+
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> tmp = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+       backtracking(n, k, 1, 0);
+       return res;
+    }
+
+    void backtracking(int n, int k, int startIndex, int sum) {
+        if (sum > n) {
+            return;
+        }
+        if (tmp.size() == k) {
+            if (sum == n) {
+                res.add(new ArrayList<>(tmp));
+            }
+            return;
+        }
+        for (int i = startIndex; i <= 9 - (k - tmp.size()) + 1; i++) {
+            tmp.add(i);
+            sum += i;
+            backtracking(n, k, ++startIndex, sum);
+            tmp.remove(tmp.size() - 1);
+            sum -= i;
+        }
+    }
+}
---
+++ leetcode/src/main/java/com/liufei/string/Top0541_simple.java
@@ -0,0 +1,49 @@
+package com.liufei.string;
+
+/**
+ * 
+ */
+public class Top0541_simple {
+
+    public static void main(String[] args) {
+        String s = "abcdefg";
+        int k = 2;
+        String s1 = reverseStr2(s, k);
+        System.out.println(s1);
+    }
+
+
+    public static String reverseStr(String s, int k) {
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        int count = (n + 2 * k - 1) / (2 * k);
+        for (int i = 0; i < count; i++) {
+            int left = i * 2 * k, right = Math.min(left + k - 1, n - 1);
+            while (left < right) {
+                char tmp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = tmp;
+                right--;
+                left++;
+            }
+        }
+        return new String(chars);
+    }
+
+    public static String reverseStr2(String s, int k) {
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        for (int i = 0; i < n; i += 2 * k) {
+            int left = i, right = Math.min(left + k - 1, n - 1);
+            while (left < right) {
+                char tmp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = tmp;
+                right--;
+                left++;
+            }
+        }
+        return new String(chars);
+    }
+
+}
